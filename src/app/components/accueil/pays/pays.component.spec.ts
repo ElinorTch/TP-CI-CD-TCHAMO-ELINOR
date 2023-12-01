f442124200f4ca709c7b3b78adeb00092a7177b4
ElinorTch <tchamoeliicode@gmail.com>
2023-12-01 07:49:05 +0100

    Tests: modification des testst du composant pays

--- src/app/components/accueil/pays/pays.component.spec.ts
+++ src/app/components/accueil/pays/pays.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClient } from '@angular/common/http';
 import { PaysComponent } from './pays.component';
 
 describe('PaysComponent', () => {
